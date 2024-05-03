bfa7aad539fa06bf8cde97d5774f774ea155baef
beto-machado <luisrobertobeto3@gmail.com>
2024-05-03 16:13:11 -0300

    :bug: adding and correcting tests

--- test/controllers/leads_controller_test.rb
+++ test/controllers/leads_controller_test.rb
@@ -23,6 +23,19 @@ class LeadsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to lead_url(Lead.last)
   end
 
+  test "should not create lead without email" do
+    post leads_url, params: { lead: { email: nil, name: @lead.name, phone: @lead.phone } }
+    assert_response :unprocessable_entity
+    assert_match "E-mail não pode ficar em branco", response.body
+    assert_match "E-mail não é válido", response.body
+  end
+
+  test "should not create lead without name" do
+    post leads_url, params: { lead: { email: @lead.email, name: nil, phone: @lead.phone } }
+    assert_response :unprocessable_entity
+    assert_match "Nome não pode ficar em branco", response.body
+  end
+
   test "should show lead" do
     get lead_url(@lead)
     assert_response :success
@@ -36,6 +49,19 @@ class LeadsControllerTest < ActionDispatch::IntegrationTest
   test "should update lead" do
     patch lead_url(@lead), params: { lead: { email: @lead.email, name: @lead.name, phone: @lead.phone } }
     assert_redirected_to lead_url(@lead)
+  end
+
+  test "should not update lead without email" do
+    patch lead_url(@lead), params: { lead: { email: nil, name: @lead.name, phone: @lead.phone } }
+    assert_response :unprocessable_entity
+    assert_match "E-mail não pode ficar em branco", response.body
+    assert_match "E-mail não é válido", response.body
+  end
+
+  test "should not update lead without name" do
+    patch lead_url(@lead), params: { lead: { email: @lead.email, name: nil, phone: @lead.phone } }
+    assert_response :unprocessable_entity
+    assert_match "Nome não pode ficar em branco", response.body
   end
 
   test "should destroy lead" do
