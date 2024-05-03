e1c8b98f6f5572c750cc930b7c1ee496b9ccf3f7
beto-machado <luisrobertobeto3@gmail.com>
2024-05-03 15:36:35 -0300

    :bug: remove config.locale in application.rb and correcting the tailwind file in application.html.erb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module TestSreC2s
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    congif.locales(brasil_america)
 
     # Configuration for the application, engines, and railties goes here.
     #
