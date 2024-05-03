c196d8cc882274128683db667a34ca56330c080a
beto-machado <luisrobertobeto3@gmail.com>
2024-05-03 15:45:51 -0300

    :bug: adding email validation and testing

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,6 @@
 class Lead < ApplicationRecord
-  validates :email, presence: true, uniqueness: false
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, uniqueness: false, format: { with: EMAIL_REGEX }
   validates :name, presence: true
 end
--- test/models/lead_test.rb
+++ test/models/lead_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class LeadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save lead with name" do
+    lead = Lead.new(name: "John Doe", email: "valid@email.com")
+    assert lead.save
+  end
+
+  test "should not save lead without name" do
+    lead = Lead.new(name: "", email: "invalid_email")
+    assert_not lead.save
+  end
+  test "should not save lead with invalid email format" do
+    lead = Lead.new(name: "John Doe", email: "invalid_email")
+    assert_not lead.save
+  end
+
+  test "should save lead with valid email format" do
+    lead = Lead.new(name: "John Doe", email: "valid@email.com")
+    assert lead.save
+  end
 end
