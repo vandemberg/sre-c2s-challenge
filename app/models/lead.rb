a18ad70fa3f7face017ef980ae9dba66c7ce609c
rafavanzo <rafacvanzo@hotmail.com>
2024-05-04 16:11:53 -0300

    adiciona validação de email e telefone

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,5 @@
 class Lead < ApplicationRecord
-  validates :email, presence: true, uniqueness: false
+  validates :email, presence: true, uniqueness: false, format: { with: URI::MailTo::EMAIL_REGEXP, message: :invalid }
   validates :name, presence: true
+  validates :phone, presence: true, format: { with: /\A\(?\d{2}\)?\s?\d{4,5}-?\d{4}\z/, message: :invalid }
 end
