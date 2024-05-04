b8520e1328b4ccb8a366d3159c92d194862b2b5a
rafavanzo <rafacvanzo@hotmail.com>
2024-05-04 15:50:32 -0300

    corrige bug ao rodar db:create e cria locales pt-BR

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module TestSreC2s
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    congif.locales(brasil_america)
+    config.i18n.default_locale = "pt-BR"
+    config.time_zone = "Brasilia"
 
     # Configuration for the application, engines, and railties goes here.
     #
